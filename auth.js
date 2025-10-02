4095834fe04d8cba68085f2595f376ce210e746c
Netanel1010 <netanel.fire@gmail.com>
2025-10-03 02:09:14 +0300

    and now?

--- auth.js
+++ auth.js
@@ -1,4 +1,15 @@
-//localStorage
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
+
 document.addEventListener('DOMContentLoaded' , ()=>{
     fetch('login.json')
     .then(res => res.json())
@@ -140,5 +151,4 @@ document.addEventListener('DOMContentLoaded' , ()=>{
     })
     .catch(error => console.error(error))
 });
-
 
